33dcd02194d2367ea1f46de75ba688a09d2932c2
ehy0419 <2hy0419@naver.com>
2025-08-01 10:52:02 +0900

    엔티티 member

---
+++ src/main/java/com/springcrudmember/SpringCrudMemberApplication.java
@@ -0,0 +1,13 @@
+package com.springcrudmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudMemberApplication.class, args);
+    }
+
+}
